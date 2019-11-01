1a9e0cf3851a204b76bfccaa006ffbad3bfdb603
Sam Moffatt <pasamio@gmail.com>
2019-10-31 22:13:36 -0700

    Add hasFeature - quick check to see if feature is enabled nor not.

--- utils.php
+++ utils.php
@@ -7,4 +7,20 @@ function generateDocumentId()
 	return 'rec-' . str_replace('-', '', UUID::mint()->string);
 }
 
+/**
+ * Check if a feature is enabled for a config.
+ *
+ * @param   array   $config   Configuration array.
+ * @param   string  $feature  Name of the feature to validate.
+ *
+ * @return  boolean  If the feature is enabled in the config.
+ */  
+function hasFeature($config, $feature)
+{
+        if (isset($config['features']))
+        {
+                return in_array($feature, array_map('trim', explode(',', $config['features'])));
+        }
+        return false;
+}
 
