f2d0156a0203ea038f7b34a95839c1344b072e0e
Sam Moffatt <pasamio@gmail.com>
2021-03-09 00:59:37 -0800

    Indenting

--- scripts/js/tampermonkey.js
+++ scripts/js/tampermonkey.js
@@ -1,13 +1,12 @@
-const fetchAsBlob = url => fetch(url)
-.then(response => response.blob());
+const fetchAsBlob = url => fetch(url) .then(response => response.blob());
 
 const convertBlobToBase64 = blob => new Promise((resolve, reject) => {
-const reader = new FileReader;
-reader.onerror = reject;
-reader.onload = () => {
+  const reader = new FileReader;
+  reader.onerror = reject;
+  reader.onload = () => {
     resolve(reader.result);
-};
-reader.readAsDataURL(blob);
+  };
+  reader.readAsDataURL(blob);
 });
 
 
